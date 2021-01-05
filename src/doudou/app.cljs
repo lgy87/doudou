e4c9ad2d7c640fa9c0e4abc56d983776c5562e1b
李光耀 <lgy87@foxmail.com>
2021-01-06 00:43:49 +0800

    [feat] react-router

--- src/doudou/app.cljs
+++ src/doudou/app.cljs
@@ -1,7 +1,11 @@
 (ns doudou.app
-  (:require ["@blueprintjs/core" :refer [Button Tabs Tab]]))
+  (:require ["@blueprintjs/core" :refer [Button Tabs Tab]]
+            ["react" :as React :refer [createElement]]
+            [reagent.core :as r]
+            ["react-router-dom" :refer [BrowserRouter Routes Route]]))
 
 (defn app []
-  [:> Tabs {:id "tabs" :selectedTabId "rx" :vertical true}
-   [:> Tab {:id "ng" :title "angular"}]
-   [:> Tab {:id "rx" :title "react"}]])
+  [:> BrowserRouter
+   [:> Routes
+    [:> Route {:path "/playgrounds/:id" :element (r/as-element [:h1 "lgy"])}]
+    [:> Route {:path "/" :element (r/as-element [:h1 "hello, welcome!"])}]]])
